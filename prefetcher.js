8b89a9bed88e4d43dda4d0d21d185623f49fea13
luizpanariello <lfilho@directtalk.com.br>
2015-05-22 16:17:26 -0300

    IE8/7

--- prefetcher.js
+++ prefetcher.js
@@ -70,6 +70,19 @@
 		var _self = this;
 		_self.listBegin();
 
+		if (typeof XMLHttpRequest === "undefined") {
+		  XMLHttpRequest = function () {
+		    try { return new ActiveXObject("Msxml2.XMLHTTP.6.0"); }
+		    catch (e) {}
+		    try { return new ActiveXObject("Msxml2.XMLHTTP.3.0"); }
+		    catch (e) {}
+		    try { return new ActiveXObject("Microsoft.XMLHTTP"); }
+		    catch (e) {}
+		    // Microsoft.XMLHTTP points to Msxml2.XMLHTTP and is redundant
+		    throw new Error("This browser does not support XMLHttpRequest.");
+		  };
+		}
+
 		var xmlhttp = new XMLHttpRequest(); 
 
 	    xmlhttp.onreadystatechange = function(data) {
